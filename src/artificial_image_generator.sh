b56938692daef93431419c64030de3604a2ef8ef
santoshreddy254 <santoshreddy45@yahoo.com>
2019-10-29 19:49:20 +0100

    Added more functions

--- src/artificial_image_generator.sh
+++ src/artificial_image_generator.sh
@@ -8,5 +8,4 @@ if [ ! -d "augmented" ]; then
   # Control will enter here if $DIRECTORY exists.
   mkdir augmented && mkdir augmented/images && mkdir augmented/labels && mkdir augmented/obj_det_labels
 fi
-python3 image_resize.py
-python3 main.py ./images/ ./semantic_labels/ --backgrounds_path ./backgrounds/ --image_save_path ./augmented/images/ --label_save_path ./augmented/labels/ --save_obj_det_label True --obj_det_save_path ./augmented/obj_det_labels/ --num_images $1 --max_objects 3 --real_img_type .png
+python3 gui_main.py
